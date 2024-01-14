7e34468e613fa2115cbb0a09934aac4827fb6a2c
Dibyanshu10 <dibyanshu.singh@resilientitservices.com>
2024-01-14 15:42:56 +0530

    New file index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let arr = ["23", false, 12];
+console.log(arr);
+
+//copying array into another array
+let newarr = [...arr, 12, "Dibyanshu"];
+console.log(newarr);
+
+// Object type in js
+
+let obj = {
+  Dibyanshu: "25",
+  Company: "Resilient IT Services",
+  Address: {
+    District: "Bhagalpur",
+    State: "Bihar",
+  },
+};
+
+//console.log(obj, typeof obj);
+console.log(Object.keys(obj));
+console.log(obj.Company);
